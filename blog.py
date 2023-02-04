265283c014bed8747bdf49b2d148e5caf50d545b
serhatarabaci59 <serhatarabaci59@gmail.com>
2023-02-04 10:31:00 +0300

    first commit

---
+++ blog.py
@@ -0,0 +1,54 @@
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+
+# kullanıcı Kayıt Formu
+class RegisterForm(Form):
+    name = StringField("İsim Soyisim",validators = [validators.Length(min = 4, max =25 )])
+    username = StringField("Kullanıcı Adı",validators = [validators.Length(min = 5, max =35 )])
+    email = StringField("E-mail Adresi",validators = [validators.Email(message= "Lütfen Geçerli Bir Mail Adresi Girin...")])
+    password = PasswordField("Parola:", validators = [
+        validators.DataRequired(message="Lütfen Parola Belirleyin"),
+        validators.EqualTo(fieldname = "confirm",message="Parolanız Uyuşmuyor")
+    ])
+    confirm = PasswordField("Parola Doğrula")
+
+#Kayıt Olma
+@app.route("/register")
+def register():
+    return render_template("register.html")
+if __name__=="__main__":
+    app.run(debug=True)
+    
+
+
+
+app = Flask(__name__)
+
+app.config["MYSQL_HOST"]= "localhost"
+app.config["MYSQL_USER"]= "root"
+app.config["MYSQL_pASSWORD"]= ""
+app.config["MYSQL_DB"]= "ybblog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
+
+
+@app.route("/")
+def index():
+    numbers = [1,2,3,4,5]
+    return render_template("index.html", numbers = numbers )
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/article/<string:id>")
+def detail(id):
+    return "Article Id:" + id
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
